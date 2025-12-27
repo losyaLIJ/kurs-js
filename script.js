5adb1da063532e679af68b2ca0fd3a38a68c9565
Topa <elisey.study.04@gmail.com>
2025-12-27 16:30:44 +0300

    вычислил произведение цифр числа и вывод первых двух цифр его куба

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("First lesson!");
+console.log("Complecated Second lesson!");
+
+const num = 266219;
+const strNum = String(num);
+let product = 1;
+
+for(let i = 0; i < strNum.length; i++) {
+    product *= Number(strNum[i]);
+}
+console.log("Произведение цифр числа " + num + ":", product);
+
+const productCubed = product ** 3;
+console.log("Куб произведения:", productCubed);
+
+const firstNumberCubed = String(productCubed)[0];
+const secondNumberCubed = String(productCubed)[1];
+console.log("Первые 2 числа этого куба:", firstNumberCubed, secondNumberCubed);
+
+
