fab9ba5067d8d33f8cffadba111199c796d23102
Topa <elisey.study.04@gmail.com>
2025-12-30 22:05:49 +0300

    объявил функцию фильтра строки

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("First lesson!");
+'use strict';
+
+const filterText = function(text){
+    if(typeof(text) != "string"){
+        return "Переданный параметр не является строкой";
+    }
+    const clearText = text.trim();
+    if(clearText.length > 30){
+        return clearText.slice(0, 30) + "...";
+    } else {
+        return clearText;
+    }
+}
