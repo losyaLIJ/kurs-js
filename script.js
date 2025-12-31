a967390e0029e6136c546f7da85580042e4dcb1a
Topa <elisey.study.04@gmail.com>
2025-12-31 14:30:00 +0300

    добавил вывод чисел начинающихся с 2 и 4, вывод простых числе от 2 до 100

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("First lesson!");
+'use strict';
+
+arr = ["123", "456", "789", "101112", "131415", "21352", "7642"];
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i][0] === "2" || arr[i][0] === "4") {
+        console.log(arr[i]);
+    }
+}
+
+for (let i = 2; i <= 100; i++) {
+    let isPrime = true;
+
+    for (let j = 2; j <= Math.sqrt(i); j++) {
+        if(i % j === 0) {
+            isPrime = false;
+            break;
+        }
+    }
+    if (isPrime) {
+        console.log(i + " Делители этого числа: 1 и " + i);
+    }
+}
