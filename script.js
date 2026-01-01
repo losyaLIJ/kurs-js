573143ba878842e5d4ad4329629b21e26bd89b0d
Topa <elisey.study.04@gmail.com>
2026-01-01 15:16:00 +0300

    Добавил усложненный вариант 'Угадайки' как отдельную функцию

--- script.js
+++ script.js
@@ -46,5 +46,55 @@ function createGame() {
     return ask;
 }
 
-const guessTheNumber = createGame();
+function createGameWithLimit() {
+    const numberToGuess = Math.floor(Math.random() * 100) + 1;
+    let attempts = 10;
+
+    function ask() {
+        if (attempts === 0) {
+            const playAgain = confirm("Попытки закончились. Хотите сыграть еще?");
+            if (playAgain) {
+                const newGame = createGameWithLimit();
+                newGame();
+            }
+            return;
+        }
+
+        const userInput = prompt("Угадай число от 1 до 100");
+
+        if (userInput === null) {
+            alert("Игра окончена");
+            return;
+        }
+        const userNumber = getNumber(userInput);
+
+        if (userNumber === null) {
+            alert("Введи число!");
+            return ask();
+        }
+        attempts--;
+
+        if (userNumber > numberToGuess) {
+            alert("Загаданное число меньше, осталось попыток: " + (attempts));
+            return ask();
+        }
+
+        if (userNumber < numberToGuess) {
+            alert("Загаданное число больше, осталось попыток: " + (attempts));
+            return ask();
+        }
+
+        const agree = confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?");
+        if (agree) {
+            const newGame = createGameWithLimit();
+            newGame();
+        }
+    }
+
+    return ask;
+}
+
+
+// const guessTheNumber = createGame();
+const guessTheNumber = createGameWithLimit();
 guessTheNumber();
