e451da2f465101605930c9c7d6dcb6cdccfae942
Topa <elisey.study.04@gmail.com>
2026-01-02 12:25:42 +0300

    написал функцию принта дней недели: выходные - курсивом, текущий - жирным

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("First lesson!");
+'use strict';
+
+const week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
+const todayIndex = new Date().getDay() === 0 ? 6 : new Date().getDay() - 1;
+
+const container = document.querySelector("body");
+
+const printDays = function () {
+    for(let i = 0; i < week.length; i++) {
+        const p = document.createElement('p');
+        p.textContent = week[i];
+        
+        if (i == todayIndex) {
+            p.style.fontWeight = 'bold';
+        }
+
+        if (i == 5 || i == 6) {
+            p.style.fontStyle = 'italic';
+        }
+        container.appendChild(p);
+    }
+}
+
+printDays();
