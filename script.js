b516185de704aea81c7682724c6bcd5f15837f1d
Topa <elisey.study.04@gmail.com>
2026-01-06 10:42:43 +0300

    Исправил отступ

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ const todayIndex = new Date().getDay() === 0 ? 6 : new Date().getDay() - 1;
 const container = document.querySelector("body");
 
 const printDays = function () {
-    for(let i = 0; i < week.length; i++) {
+    for (let i = 0; i < week.length; i++) {
         const p = document.createElement('p');
         p.textContent = week[i];
-        
+
         if (i == todayIndex) {
             p.style.fontWeight = 'bold';
         }
