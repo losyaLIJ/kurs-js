8dbf92da0c2dbd69ffd5056d8e4e36afa44a8d70
Topa <elisey.study.04@gmail.com>
2026-01-11 16:23:09 +0300

    добавил обработчик на кнопку для замены цвета квадрата, на ползунок - для изменения размеры круга, скрыл кнопку внутри круга

--- script.js
+++ script.js
@@ -1,90 +1,27 @@
 'use strict';
 
-let appData = {
-    title: "",
-    screens: "",
-    screenPrice: 0,
-    adaptive: false,
-    rollback: 10,
-    allServicePrices: 0,
-    fullPrice: 0,
-    servicePercentPrice: 0,
-    service1: "",
-    service2: "",
-    start: function () {
-        appData.asking();
-        appData.allServicePrices = appData.getAllServicePrices();
-        appData.fullPrice = appData.getFullPrice();
-        appData.servicePercentPrice = appData.getServicePercentPrice();
-        appData.title = appData.getTitle();
+const btn = document.getElementById('btn');
+const text = document.getElementById('text');
+const square = document.getElementById('square');
+const circle = document.getElementById('circle');
+const circleBtn = document.getElementById('e_btn');
+circleBtn.style.display = 'none';
+const range_span = document.getElementById('range-span');
+const range = document.getElementById('range');
 
-        appData.logger();
-    },
-    logger: function () {
-        for (let key in appData) {
-            console.log(key + ": " + appData[key]);
-        }
-    },
-    asking: function () {
-        appData.title = prompt("Как называется ваш проект?", "Калькулятор верстки");
-        appData.screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
-        appData.screenPrice = appData.getNumber("Сколько будет стоить данная работа?");
-        appData.adaptive = confirm("Нужен ли адаптив на сайте?");
-    },
-    getNumber: function (message) {
-        let value;
+btn.addEventListener('click', () => {
+    const color = text.value.trim();
+    if (!color) return;
 
-        do {
-            value = prompt(message);
-            if (value === null) {
-                return null;
-            }
+    square.style.backgroundColor = color;
+})
 
-            value = value.trim();
-        } while (value === "" || !isFinite(value))
+range.addEventListener('input', () => {
+    const size = range.value;
+    range_span.textContent = size;
+    circle.style.width = size + '%';
+    circle.style.height = size + '%';
+});
 
-        return Number(value);
-    },
-    getAllServicePrices: function () {
-        let sum = 0;
-        let priceService = 0;
 
-        for (let i = 0; i < 2; i++) {
-            appData[`service${i + 1}`] = prompt("Какой дополнительный тип услуги нужен?");
 
-            priceService = appData.getNumber("Сколько это будет стоить?");
-            if (priceService === null) {
-                i--;
-                continue;
-            }
-            sum += priceService;
-        }
-
-        return sum;
-    },
-    getFullPrice: function () {
-        return appData.screenPrice + appData.allServicePrices;
-    },
-    getServicePercentPrice: function () {
-        return appData.fullPrice - (appData.fullPrice * (appData.rollback / 100));
-    },
-    getTitle: function () {
-        const clearTitle = appData.title.trim();
-        return clearTitle
-            ? clearTitle[0].toUpperCase() + clearTitle.slice(1).toLowerCase()
-            : clearTitle;
-    },
-    getRollbackMessage: function (price) {
-        if (price >= 30000) {
-            return "Даем скидку в 10%";
-        } else if (price >= 15000 && price < 30000) {
-            return "Даем скидку в 5%";
-        } else if (price < 15000 && price >= 0) {
-            return "Скидка не предусмотрена";
-        } else {
-            return "Что-то пошло не так";
-        }
-    },
-}
-
-appData.start();
